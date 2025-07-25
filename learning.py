cd3ee9c9939b754917ed9f98aedea23982cca98b
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-07-25 21:42:11 +0300

    Update learning.py

--- learning.py
+++ learning.py
@@ -16,6 +16,9 @@ print("Never mind on such..");
 def our_function():
     print("Still on Progress")
 
+def here_function():
+    print(')
+
 print("Big dreams for sure begin with the small atomic habits..")
 print('Continous growth and improvement is the core wish of any peer with good will...we for sure need good and reliable friends!!')
 print("Mathematics and atrithmetic operations play a key role in coding");
